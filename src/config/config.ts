eff684c00e727373b7f4a1db527d63d1ec5ecebe
Lester <lesterdeguzman1999@gmail.com>
2025-07-11 12:32:55 +0800

    feat: Update config mongo_uri by referencing env variable

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,15 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 const PORT = Number(process.env.PORT) || 8000;
-const HOST = process.env.HOST || "localhost";
+const HOST = process.env.HOST || 'localhost';
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_URI ='mongodb://localhost:27017/attendance-payroll';
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "";
-const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "";
+const MONGO_URI =
+  process.env.MONGO_URI || 'mongodb://localhost:27017/attendance-payroll';
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || '';
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || '';
 
 export const config = {
   port: PORT,
@@ -16,6 +17,6 @@ export const config = {
   dbUri: MONGO_URI,
   accessTokenPrivateKey: ACCESS_TOKEN_SECRET,
   refreshTokenPrivateKey: REFRESH_TOKEN_SECRET,
-  accessTokenExpire: "3m",
-  refreshTokenExpire: "15d",
+  accessTokenExpire: '3m',
+  refreshTokenExpire: '15d',
 };
