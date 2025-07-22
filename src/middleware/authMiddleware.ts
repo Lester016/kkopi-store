218f71951e7edc9ee9572d3e470d9712e178151d
Lester <lesterdeguzman1999@gmail.com>
2025-07-22 12:19:41 +0800

    feat: Update auth middleware assigning of user data

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,35 +1,38 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
-import asyncHandler from "express-async-handler"; // returns the error via json
-import { config } from "../config/config.js";
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
+import asyncHandler from 'express-async-handler'; // returns the error via json
+import { config } from '../config/config.js';
+import { DecodedToken } from '../types/token.js';
 
 const protect = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     let token;
     if (
       req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
+      req.headers.authorization.startsWith('Bearer')
     ) {
       try {
-        token = req.headers.authorization.split(" ")[1]; // get the token.
+        token = req.headers.authorization.split(' ')[1]; // get the token.
 
         // verifying the token here.
-        const decoded = jwt.verify(token, config.accessTokenPrivateKey);
+        const decoded = jwt.verify(
+          token,
+          config.accessTokenPrivateKey
+        ) as DecodedToken;
 
         // fetch user data remove hashed password to queries.
-        req.user = decoded;
-        console.log("Decoded Token Data", decoded);
+        req.user = decoded.user;
 
         next();
       } catch (error) {
         console.error(error);
-        res.status(401).send({ error: "Not authorized, Invalid token" });
+        res.status(401).send({ error: 'Not authorized, Invalid token' });
       }
     }
 
     if (!token) {
-      res.status(401).send({ error: "Not authorized, no token found" }); // Unauthorized
+      res.status(401).send({ error: 'Not authorized, no token found' }); // Unauthorized
     }
   }
 );
