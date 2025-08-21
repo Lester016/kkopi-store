2c699780da7f6b7143394182bd8895842747d186
Lester <lesterdeguzman1999@gmail.com>
2025-08-21 08:21:45 +0800

    feat: Update deployment pipeline to point to ecosystem config in the server instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,8 @@ module.exports = {
       script: 'npm',
       args: 'run start',
       cwd: '/home/ubuntu/kkopi-store/deployment/current',
+      // env_file: "/home/ubuntu/kkopi-store/deployment/config/.env", // ✅ load env
       watch: false,
-      env: {
-        NODE_ENV: 'production',
-      },
     },
   ],
 };
